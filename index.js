9f4dd7475c6afdf077c069139cbed668ec82c69c
Jody Hestick <jhestick@gmail.com>
2018-04-09 20:03:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  const main = document.querySelector("main")
+  const nestedId = main.querySelector("#nested")
+  const target = nestedId.querySelector(".target")
+  return target
+}
+
+function increaseRankBy(n){
+  var lis = document.getElementById("app").querySelectorAll('ul.ranked-list li')
+  
+  for (var i=0; i<lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild(){
+  var parent = document.querySelector("div#grand-node")
+  var firstChild = parent.children[0]
+  while(firstChild){
+    if (firstChild.children[0]){
+      firstChild = firstChild.children[0]
+    } else {
+      return firstChild
+    }
+  }
+}
+
